fe0d99a36d20f959b8a7227730b22140e3b6c183
Apple <apple@Apples-Air.lan>
2026-04-28 19:50:56 +0530

    Fix: Make Cloudinary config build-safe

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,13 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-if (!process.env.CLOUDINARY_CLOUD_NAME) {
-  throw new Error('CLOUDINARY_CLOUD_NAME is not set');
-}
-
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
+  api_key: process.env.CLOUDINARY_API_KEY || "",
+  api_secret: process.env.CLOUDINARY_API_SECRET || "",
   secure: true,
 });
 
